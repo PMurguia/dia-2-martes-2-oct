c0085397615b8a8ce82bf7a382a2cda586d8a47b
Peio Murguia <peiomurguia@gmail.com>
2018-10-02 10:04:31 +0200

    Otra subida más

--- Condicionales-prueba/Condicionales-prueba/Program.cs
+++ Condicionales-prueba/Condicionales-prueba/Program.cs
@@ -46,37 +46,44 @@ namespace Condicionales_prueba
             }
             Console.ReadLine();
 
-            int numSem;
-            Console.WriteLine("Dime un número del 1 al 7.");
-            numSem = Int32.Parse(Console.ReadLine());
 
-            if (numSem == 1)
+            //Pedimos un número del 1 al 7 y lo traducimos al día de la semana correspondiente
+
+            int numSem; //establecemos variable
+            Console.WriteLine("Dime un número del 1 al 7."); //Mensaje en pantalla
+            numSem = Int32.Parse(Console.ReadLine()); //Guardamos el valor dado por el usuario en la variable
+
+            if (numSem == 1) //Si el número es un uno
             {
-                Console.WriteLine("Ese número corresponde al lunes.");
+                Console.WriteLine("Ese número corresponde al lunes."); //Salta este mensaje
             }
-            else if (numSem == 2)
+            else if (numSem == 2) //Si el número es un dos
             {
-                Console.WriteLine("Ese número corresponde al martes.");
+                Console.WriteLine("Ese número corresponde al martes."); //Salta este mensaje
             }
-            else if (numSem == 3)
+            else if (numSem == 3) //Si el número es un tres
             {
-                Console.WriteLine("Ese número corresponde al miércoles.");
+                Console.WriteLine("Ese número corresponde al miércoles."); //Salta este mensaje
             }
-            else if (numSem == 4)
+            else if (numSem == 4) //Si el número es un cuatro
             {
-                Console.WriteLine("Ese número corresponde al jueves.");
+                Console.WriteLine("Ese número corresponde al jueves."); //Salta este mensaje
             }
-            else if (numSem == 5)
+            else if (numSem == 5) //Si el número es un cinco
             {
-                Console.WriteLine("Ese número corresponde al viernes.");
+                Console.WriteLine("Ese número corresponde al viernes."); //Salta este mensaje
             }
-            else if (numSem == 6)
+            else if (numSem == 6) //Si el número es un seis
             {
-                Console.WriteLine("Ese número corresponde al sábado.");
+                Console.WriteLine("Ese número corresponde al sábado."); //Salta este mensaje
             }
-            else
+            else if (numSem == 7) //Si el número es un siete
             {
-                Console.WriteLine("Ese número corresponde al domingo.");
+                Console.WriteLine("Ese número corresponde al domingo."); //Salta este mensaje
+            }
+            else //Si han introducido un número que no está dentro del rango solicitado
+            {
+                Console.WriteLine("No es un número del 1 al 7."); //Salta este mensaje
             }
             Console.ReadLine();
         }
